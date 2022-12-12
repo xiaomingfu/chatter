87833d8e6ebd4c640ab26f49ad81bbcde0db95dd
Xiaoming Fu <xmfswe@gmail.com>
2022-12-12 12:01:12 -0800

    Add code to prep for interview

---
+++ interview.js
@@ -0,0 +1,33 @@
+`
+Hi, thanks for having the interview with me. Pls paste the question statement below:
+
+
+
+
+`;
+
+/// Solution
+function solution() {}
+
+/// Testing
+(function test() {
+  const testCases = [
+    [[1, 2, 3], 5],
+    [[1, 2, 3], 5],
+  ];
+
+  for (const testCase of testCases) {
+    const result = solution(...testCase);
+
+    console.log(`
+Case: ${JSON.stringify(testCase)}
+Result: ${JSON.stringify(result)}
+`);
+  }
+})();
+
+/// Analysis
+`
+Analysis:
+
+`;
